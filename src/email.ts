8a811c24093a1ec010039ee01a23b45704fb9971
Harold Martin <harold.martin@gmail.com>
2024-10-31 15:46:28 -0700

    use worker types, replace discord with logging, replace letterparser with postal-mime

--- src/email.ts
+++ src/email.ts
@@ -1,29 +1,13 @@
-import { extract as parseRawEmail } from 'letterparser';
+import PostalMime from 'postal-mime';
+import type { Env } from './types';
 
-export async function email(message: any, env: any, ctx?: any): Promise<void> {
-  const url = env.DISCORD_WEBHOOK_URL;
-  const DEFAULT_MAX_LENGTH = 524288; // 512KB
-  const maxLenFromEnv = parseInt(env.MAX_LEN);
-  const MAX_LEN: number = !isNaN(maxLenFromEnv) && maxLenFromEnv > 0 
-    ? maxLenFromEnv 
-    : DEFAULT_MAX_LENGTH;
-  if (!url) throw new Error('Missing DISCORD_WEBHOOK_URL');
-
-  // Parse email
+export async function email(message: ForwardableEmailMessage, env: Env, ctx?: ExecutionContext): Promise<void> {
+  const MAX_LEN = 524288; // 512KB
   const { from, to } = message;
-  // BugFix: Replace "UTF-8" with "utf-8" to prevent letterparser from throwing an error for some messages.
-  const rawEmail = (await new Response(message.raw).text()).replace(/utf-8/gi, 'utf-8');
-
-  // https://github.com/mat-sz/letterparser
-  const email = parseRawEmail(rawEmail);
-  const subject = message.headers.get('subject') || email.subject;
-  const body = email.text?.substring(MAX_LEN);
-
-  // Send discord message
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ body, subject, to, from }),
-  });
-  if (!response.ok) throw new Error('Failed to post message to Discord webhook.' + (await response.json()));
+  const email = await PostalMime.parse(message.raw);
+  const subject = email.subject;
+  const body = email.text?.substring(0, MAX_LEN);
+  console.log(from);
+  console.log(subject);
+  console.log(body);
 }
